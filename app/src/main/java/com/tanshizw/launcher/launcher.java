b949888c7de65cab5cbd57f299e879dfb79bc48c
Tan Shi <shi.tan@t2mobile.com>
2016-05-25 17:52:33 +0800

    Initial commit

---
+++ app/src/main/java/com/tanshizw/launcher/launcher.java
@@ -0,0 +1,13 @@
+package com.tanshizw.launcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
