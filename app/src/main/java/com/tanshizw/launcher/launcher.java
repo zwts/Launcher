14cc781671647efde9ffb52765b005526a16c040
Tan Shi <shi.tan@t2mobile.com>
2016-05-27 18:33:02 +0800

    Empty Page with no title

--- app/src/main/java/com/tanshizw/launcher/launcher.java
+++ app/src/main/java/com/tanshizw/launcher/launcher.java
@@ -1,9 +1,9 @@
 package com.tanshizw.launcher;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class launcher extends AppCompatActivity {
+public class launcher extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
